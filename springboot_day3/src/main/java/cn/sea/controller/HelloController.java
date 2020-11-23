9ea56d459b46f6142c694f67a23f6cf683eb7eba
luxianghai <3243016771@qq.com>
2020-11-23 19:02:11 +0800

    springboot_day3: springboot注入元数据

--- springboot_day3/src/main/java/cn/sea/controller/HelloController.java
+++ springboot_day3/src/main/java/cn/sea/controller/HelloController.java
@@ -1,16 +1,61 @@
 package cn.sea.controller;
 
+import cn.sea.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
+    @Value("${name}")  // 将springboot配置文件中的 name 注入到该属性中
+    private String name;
+    // 注入springboot配置文件中的 day3.url 属性
+    @Value("${day3.url}")
+    private String url;
+    @Value("${server.port}") // 注入springboot配置文件中的 server.port 属性
+    private String port;
+    // 注入日期数据
+    @Value("${day3.date1}")
+    private Date date1;
+    @Value("${day3.date2}")
+    private Date date2;
+    // 注入数组
+    @Value("${day3.names}")
+    private String[] names;
+    // 注入list集合
+    @Value(("${day3.lists}"))
+    private List<String> lists;
+    // 注入 map 集合
+    @Value("#{${day3.maps}}")
+    private Map<String, String> maps;
+
+    @Autowired // 注入对象
+    private User user;
+
     @GetMapping("/hello")
     public String hello() {
         System.out.println("hello springboot !!!");
+        System.out.println("name = " + name);
+        System.out.println("url = " + url);
+        System.out.println("port = " + port);
+        System.out.println("date1 = " + date1);
+        System.out.println("date2 = " + date2);
+        for( String s: names ) {
+            System.out.println("names = " + s);
+        }
+        lists.forEach(s -> System.out.println("lists = " + s));
+        maps.forEach( (k,v) -> System.out.println("k = " + k + " ,v = " + v));
+
+        System.out.println("=====================================");
+        System.out.println("user = " + user);
         return "hello springboot !!!";
     }
 }
---
+++ springboot_day3/src/main/java/cn/sea/entity/User.java
@@ -0,0 +1,44 @@
+package cn.sea.entity;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@ConfigurationProperties(prefix = "user")// 将前缀为 user 的数据相应的注入到各个属性中
+public class User {
+
+   /* @Value("${user.name}")
+    private String name;
+    @Value("${user.age}")
+    private Integer age;*/
+
+   @Value("${name}")
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
