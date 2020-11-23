218534386d73d2ff92a6cd54e6868c8d106b9a73
luxianghai <3243016771@qq.com>
2020-11-23 15:50:36 +0800

    第一个springboot项目springboot_day1，finished

---
+++ springboot_day1/src/main/java/cn/sea/controller/HelloController.java
@@ -0,0 +1,16 @@
+package cn.sea.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        System.out.println("hello springboot !!!");
+        return "hello springboot";
+    }
+}
